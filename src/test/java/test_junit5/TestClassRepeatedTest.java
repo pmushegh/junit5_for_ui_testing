6d1cd8ebbec5ce12392b335954979f2c52dd91d7
Mushegh Petrosyan <p.mushegh@gmail.com>
2018-01-15 18:56:49 +0400

    Added new test file.

---
+++ src/test/java/test_junit5/TestClassRepeatedTest.java
@@ -0,0 +1,15 @@
+package test_junit5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInfo;
+
+public class TestClassRepeatedTest {
+
+
+    @RepeatedTest(value = 3, name = "{displayName} -> {currentRepetition}/{totalRepetitions}")
+    @DisplayName("Repeated test name")
+    public void someTest(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+}
